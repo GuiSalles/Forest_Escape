8a4ec525f87bf140ecfecd316f8cbb36a3c41baa
Guilherme Xavier Salles <34552779+GuiSalles@users.noreply.github.com>
2024-07-24 14:03:54 -0300

    Update fila_prioridade.cpp

--- src/fila_prioridade.cpp
+++ src/fila_prioridade.cpp
@@ -1,108 +1,70 @@
-#include "fila_prioridade.hpp"
-#include <algorithm>
+#include "fila_de_prioridade.hpp"
+#include <utility>
 
-const double FilaPrioridade::INFINITO = 1e10;
-
-FilaPrioridade::FilaPrioridade(int tamanho) : tamanho(tamanho), tamanhoAtual(0) {
-    prioridades = new double[tamanho];
-    vertices = new int[tamanho];
-    posicao = new int[tamanho];
-    for (int i = 0; i < tamanho; i++) {
-        posicao[i] = -1;
-        prioridades[i] = INFINITO;
-    }
+template <typename Item>
+fila_de_prioridade<Item>::fila_de_prioridade(int capacidade) 
+    : tamanho(0), capacidade(capacidade) {
+    dados = new Item[capacidade];
 }
 
-FilaPrioridade::~FilaPrioridade() {
-    delete[] prioridades;
-    delete[] vertices;
-    delete[] posicao;
+template <typename Item>
+fila_de_prioridade<Item>::~fila_de_prioridade() {
+    delete[] dados;
 }
 
-void FilaPrioridade::inserir(int vertice, double prioridade) {
-    if (posicao[vertice] != -1) {
-        atualizarPrioridade(vertice, prioridade);
-        return;
-    }
-
-    prioridades[tamanhoAtual] = prioridade;
-    vertices[tamanhoAtual] = vertice;
-    posicao[vertice] = tamanhoAtual;
-    heapifyUp(tamanhoAtual);
-    tamanhoAtual++;
+template <typename Item>
+void fila_de_prioridade<Item>::inserir(const Item& value) {
+    dados[tamanho] = value;
+    subir(tamanho++);
 }
 
-int FilaPrioridade::extrairMin() {
-    if (vazia()) return -1;
-
-    int minVertice = vertices[0];
-    posicao[minVertice] = -1;
-
-    tamanhoAtual--;
-    if (!vazia()) {
-        vertices[0] = vertices[tamanhoAtual];
-        prioridades[0] = prioridades[tamanhoAtual];
-        posicao[vertices[0]] = 0;
-        heapifyDown(0);
-    }
-    return minVertice;
+template <typename Item>
+Item fila_de_prioridade<Item>::remover() {
+    Item raiz = dados[0];
+    dados[0] = dados[--tamanho];
+    descer(0);
+    return raiz;
 }
 
-bool FilaPrioridade::vazia() const {
-    return tamanhoAtual == 0;
+template <typename Item>
+bool fila_de_prioridade<Item>::vazia() const {
+    return tamanho == 0;
 }
 
-void FilaPrioridade::atualizarPrioridade(int vertice, double novaPrioridade) {
-    int index = posicao[vertice];
-    if (index == -1) return;
-
-    double prioridadeAntiga = prioridades[index];
-    prioridades[index] = novaPrioridade;
-
-    if (novaPrioridade < prioridadeAntiga) {
-        heapifyUp(index);
-    } else {
-        heapifyDown(index);
-    }
-}
-
-void FilaPrioridade::heapifyUp(int index) {
+template <typename Item>
+void fila_de_prioridade<Item>::subir(int index) {
     while (index > 0) {
         int pai = (index - 1) / 2;
-        if (prioridades[index] < prioridades[pai]) {
-            std::swap(prioridades[index], prioridades[pai]);
-            std::swap(vertices[index], vertices[pai]);
-            posicao[vertices[index]] = index;
-            posicao[vertices[pai]] = pai;
-            index = pai;
-        } else {
-            break;
-        }
+        if (dados[index] >= dados[pai]) break;
+        trocar(index, pai);
+        index = pai;
     }
 }
 
-void FilaPrioridade::heapifyDown(int index) {
+template <typename Item>
+void fila_de_prioridade<Item>::descer(int index) {
     while (true) {
-        int esquerda = 2 * index + 1;
-        int direita = 2 * index + 2;
+        int filhoEsq = 2 * index + 1;
+        int filhoDir = 2 * index + 2;
         int menor = index;
 
-        if (esquerda < tamanhoAtual && prioridades[esquerda] < prioridades[menor]) {
-            menor = esquerda;
+        if (filhoEsq < tamanho && dados[filhoEsq] < dados[menor]) {
+            menor = filhoEsq;
         }
-
-        if (direita < tamanhoAtual && prioridades[direita] < prioridades[menor]) {
-            menor = direita;
+        if (filhoDir < tamanho && dados[filhoDir] < dados[menor]) {
+            menor = filhoDir;
         }
-
-        if (menor != index) {
-            std::swap(prioridades[index], prioridades[menor]);
-            std::swap(vertices[index], vertices[menor]);
-            posicao[vertices[index]] = index;
-            posicao[vertices[menor]] = menor;
-            index = menor;
-        } else {
-            break;
-        }
+        if (menor == index) break;
+        trocar(index, menor);
+        index = menor;
     }
 }
+
+template <typename Item>
+void fila_de_prioridade<Item>::trocar(int i, int j) {
+    Item temp = dados[i];
+    dados[i] = dados[j];
+    dados[j] = temp;
+}
+
+template class fila_de_prioridade<std::pair<double, std::pair<int, int>>>;
